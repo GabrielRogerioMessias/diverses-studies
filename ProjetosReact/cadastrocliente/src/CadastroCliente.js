daf22f94c8e8e618c8b2820c7c6348023ed38fb6
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-03-06 20:30:10 -0300

    implementando json server

--- ProjetosReact/cadastrocliente/src/CadastroCliente.js
+++ ProjetosReact/cadastrocliente/src/CadastroCliente.js
@@ -3,12 +3,15 @@ import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { Calendar } from 'primereact/calendar';
+import { Dropdown } from 'primereact/dropdown';
 
 function CadastroCliente() {
-    const [nome, setNome] = useState('');
-    const [endereco, setEndereco] = useState('');
+    const [nome, setNome] = useState('')
+    const [endereco, setEndereco] = useState('')
     const [email, setEmail] = useState('')
     const [dataNascimento, setDataNascimento] = useState('')
+    const [cidadeSelecionada, setCidadeSelecionada] = useState('')
+
     const coletarNome = (event) => {
         setNome(event.target.value)
         console.log('name: ' + nome)
@@ -20,25 +23,33 @@ function CadastroCliente() {
     const enviarDados = () => {
         console.log('Enviando dados para o banco de Dadoss')
         console.log('' + nome)
-
     }
+    const cidades = [
+        { label: 'Ourinhos', value: 'OU' },
+        { label: 'Joaquim-Tavorá', value: 'JT' },
+        { label: 'Santo-Antônio', value: 'SAP' }
+    ]
 
     return (
-        <div class="principal" style={{ margin: '0 20%' }}>
+        <div className="principal" style={{ margin: '0 20%' }}>
             <h1 style={{ textAlign: 'center' }}>Cadastro de Clientes</h1>
-            <Card >
+            <Card>
                 <div className="cardPrincipal">
-                    <label> Nome </label>
-                    <InputText id="nome" value={nome} onChange={event => { setNome(event.target.value) }} />
-                    <label>Endereco</label>
-                    <InputText id="endereco" value={endereco} onChange={coletarEndereco} />
-                    <label>Email</label>
-                    <InputText id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                    <label>Data de Nascimento</label>
-                    <Calendar id="dataNascimento" value={dataNascimento} onChange={(e) => { setDataNascimento(e.target.value) }} />
-                    <Button label="Enviar" />
-                </div>
+                    <label className='labelInput'> Nome </label>
+                    <InputText className="input" value={nome} onChange={event => { setNome(event.target.value) }} />
 
+                    <label className='labelInput'>Endereco</label>
+                    <InputText className="input" value={endereco} onChange={coletarEndereco} />
+
+                    <label className='labelInput'> Email</label>
+                    <InputText className="input" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+
+                    <Dropdown value={cidadeSelecionada} options={cidades} onChange={(e) => { setCidadeSelecionada(e.value) }} placeholder="Selecione a Cidade" />
+
+                    <label className='labelInput'> Data de Nascimento</label>
+                    <Calendar id="dataNascimento" value={dataNascimento} onChange={(e) => { setDataNascimento(e.target.value) }} />
+                    <Button label="Enviar" onClick={enviarDados} />
+                </div>
             </Card>
         </div>
     );
