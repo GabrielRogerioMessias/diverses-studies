9d58772fcaabed1ca77ba884d6d4803b8336f1fc
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-04-11 22:23:43 -0300

    atualizando gerador de senha com caracteres aleatórios

--- ProjetosReact/cadastro/src/pages/register/GeneratePassword.jsx
+++ ProjetosReact/cadastro/src/pages/register/GeneratePassword.jsx
@@ -1,18 +1,47 @@
-import './Cadastro.css'
-import { InputText } from 'primereact/inputtext';
+import { useState } from 'react';
+import './Cadastro.css';
+import { Button } from 'primereact/button';
 function GeneratePassword() {
+    const [numberOfCharacters, setNumberOfCharacters] = useState(5);
+    const [passwordGenerated, setPasswordGenerated] = useState('');
+    const [viewPassword, setViewPassword] = useState(false)
+
+    function getValue(event) {
+        setNumberOfCharacters(event.target.value)
+    }
+
+    function GeneratePassword() {
+        const password = generateRandomPassword(numberOfCharacters)
+        setPasswordGenerated(password)
+        setViewPassword(true)
+    }
+    function generateRandomPassword(quantityOfCharecters) {
+        var charset = 'abcdefghijklmnopqrstuv123456789';
+        var password = '';
+        for (let i = 0; i < quantityOfCharecters; i++) {
+            var indexCharset = Math.floor(Math.random() * charset.length)
+            password += charset[indexCharset]
+        }
+        return password;
+    }
 
     return (
         <main className='generate-password-container'>
             <div className='container-components'>
                 <h1 className='title-components'>Password Generator</h1>
-                <span>
-                    <InputText ></InputText>
-                    <label htmlFor="Digite"></label>
-                </span>
-
+                <h1>Select the number os characters</h1>
+                <input type='range' value={numberOfCharacters} min={5} max={20} onChange={getValue}></input>
+                <p>{numberOfCharacters} characters</p>
+                <Button onClick={GeneratePassword}>Generate</Button>
+                {viewPassword && (
+                    < div className='result-components'>
+                        <h1>Password:</h1>
+                        <p>{passwordGenerated}</p>
+                    </div>
+                )
+                }
             </div>
-        </main>
+        </main >
     )
 }
 export default GeneratePassword;
